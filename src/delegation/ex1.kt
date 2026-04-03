90a42a4dff9ee535349be1c767afc4e8b3df083d
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2026-04-03 11:44:55 +0900

    위임 1

---
+++ src/delegation/ex1.kt
@@ -0,0 +1,42 @@
+package delegation //위임
+
+// 클래스 위임
+// 특정 인스턴스의 메서드 호출 시 해당 기능을 다른 클래스에게 위임함
+
+interface EnhanceAnimal {
+    fun think()
+}
+
+class Human : EnhanceAnimal {
+    init {
+        println("human constructure")
+    }
+    override fun think() {
+        println("human thinking...")
+    }
+}
+
+
+class Child(private val human: Human) : EnhanceAnimal {
+    override fun think() {
+        human.think()
+    }
+}
+
+//by를 사용한 클래스 위임 1
+class ByChild1(private val human: Human) : EnhanceAnimal by human
+
+//by를 사용한 클래스 위임 2
+class ByChild2 : EnhanceAnimal by Human()
+
+fun main() {
+    val parent = Human()
+    val child = Child(parent)
+    child.think()
+
+    val byChild1 = ByChild1(parent)
+    byChild1.think()
+    val byChild2 = ByChild2()
+    byChild2.think()
+
+}
